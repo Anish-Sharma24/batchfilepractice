58ad5999ae40f8c3691f76d51e693139abff1496
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-06-30 05:52:01 +0530

    complete

--- 15_Batch_Script_Array.bat
+++ 15_Batch_Script_Array.bat
@@ -1,18 +1,18 @@
 @echo off
 Title How to deal with array using batch script
-set a=Suman Anish Abin Mrinmay Pallab Sagar
+set a=Suman Anish Sagar Mrinmay Pallab Abin
 ::for %%b in (%a%) do (
 ::    echo %%b
 ::    echo Here is the winner.
 ::    timeout /t 5 /nobreak)
 ::pause
 for %%b in (%a%) do (
-    if %%b==Sagar (echo %%b is 1st)
-    if %%b==Pallab (echo %%b is 2nd)
-    if %%b==Mrinmay (echo %%b is 3rd)
-    if %%b==Abin (echo %%b is 4th)
-    if %%b==Anish (echo %%b is 5th)
-    if %%b==Suman (echo %%b is 6th)
+    if %%b == Sagar (echo %%b is 1st)
+    if %%b == Pallab (echo %%b is 2nd)
+    if %%b == Mrinmay (echo %%b is 3rd)
+    if %%b == Abin (echo %%b is 4th)
+    if %%b == Anish (echo %%b is 5th)
+    if %%b == Suman (echo %%b is 6th)
     timeout /t 2 /nobreak
 )
 pause
