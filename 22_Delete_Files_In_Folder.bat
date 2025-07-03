aa9cfe94dcccfee347776110978ab473c0e2c50d
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-07-03 14:27:10 +0530

    .

--- 22_Delete_Files_In_Folder.bat
+++ 22_Delete_Files_In_Folder.bat
@@ -1,1 +1,5 @@
 :: HOW TO DELETE FILES USING BATCH SCRIPT
+@echo off
+ipconfig /all >> IPdetailsdelete.csv
+del "C:\Users\Admin\Downloads\Batch_Scripting\batchfilepractice\IPdetailsdelete.csv"
+pause  
