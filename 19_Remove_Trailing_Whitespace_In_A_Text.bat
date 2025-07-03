181b07b780be828241d7adf2f6a3f5470656bb8e
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-07-03 10:45:17 +0530

    Completed

---
+++ 19_Remove_Trailing_Whitespace_In_A_Text.bat
@@ -0,0 +1,11 @@
+:: TRIM WHITESPACE FROM TEXT
+@echo off
+set a= Hel lo
+set b= Anish Sharma
+echo =========================================
+echo Original: %a%
+echo Output: %a: =%
+echo =========================================
+echo Original: %b%
+echo Output : %b: S=%
+pause
