d15f39b27785ce3823c0fb412f0c9c1272f07485
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-07-04 16:49:58 +0530

    Completed.....

--- 30_Backup_Files.bat
+++ 30_Backup_Files.bat
@@ -1,4 +1,6 @@
 @echo off
-echo Backing up the Files
-cd C:\Users\Admin\Downloads\Testing Wallpapers
-for %%a in ()
+echo Backing up the Files....
+cd C:\Users\Admin\Downloads\Batch_Scripting\batchfilepractice
+for %%a in (*.bat) do copy %%a "C:\Users\Admin\Downloads\Batch_Scripting\batchfilepractice\Backup Folder"
+echo Backing Up is complete
+pause
