769bc4578e04e4560ed59302659544f3d06e2abe
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-07-07 00:06:51 +0530

    Incomplete...

--- 31_Backup_File_and_Folders.bat
+++ 31_Backup_File_and_Folders.bat
@@ -1,5 +1,8 @@
 @echo off
 Title How to backup files in folder and sub folders...
 echo deleting files
-for /R "C:\Users\Jeet Sharma\Documents\batchfilepractice\Root_Folder" %%a in (*.*) do copy %%a "C:\Users\Jeet Sharma\Documents\batchfilepractice\Backup_Folder"
-if %errorlevel%==0 echo backup completed successfully
+for /R C:\Users\Jeet Sharma\Documents\batchfilepractice\Root %%A in (*.*) do copy %%A C:\Users\Jeet Sharma\Documents\batchfilepractice\Backup
+if %errorlevel%==0 echo Backup Completed Successfully
+if %errorlevel% neq 0 echo Backup is Failed
+pause
+cmd/k
