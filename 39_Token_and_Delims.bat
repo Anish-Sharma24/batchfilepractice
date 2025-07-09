5dde632dcaa0f18d54cfb8aaa27d9d922910881e
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-07-09 10:42:26 +0530

    Completed..

--- 39_Token_and_Delims.bat
+++ 39_Token_and_Delims.bat
@@ -1,7 +1,7 @@
 :: Intro to For /f Tokens and Delims
 @echo off 
 :: will use switch F to deal with any file contents.
-cd C:\Users\Jeet Sharma\Documents\batchfilepractice
+cd C:\Users\Admin\Downloads\Batch_Scripting\batchfilepractice
 for /f "Tokens=1,3 Delims=," %%g in (Informations.txt) do (
     echo %%g %%h
 )
