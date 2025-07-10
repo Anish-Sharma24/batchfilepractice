c1177bea4284ed1826d3943f155b88c683d14296
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-07-10 18:55:29 +0530

    Completed....

--- 40_Command_For_Copying_Files.bat
+++ 40_Command_For_Copying_Files.bat
@@ -1,1 +1,7 @@
 :: Copy files listed in a text file to a new destination folder..
+@echo off
+cd C:\Users\Admin\Downloads\Batch_Scripting\batchfilepractice
+
+for /f "Delims=" %%g in (MyText.txt) do (
+    copy "C:\Users\Admin\Downloads\Batch_Scripting\Source\%%g" "C:\Users\Admin\Downloads\Batch_Scripting\Backup"
+)
